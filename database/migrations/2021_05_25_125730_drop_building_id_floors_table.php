05d36bdb685848f51dd12e463a523b01658f1e1a
Dionlee Uy <dionleeuy@gmail.com>
2021-08-09 18:13:38 +0800

    Migration fix

--- database/migrations/2021_05_25_125730_drop_building_id_floors_table.php
+++ database/migrations/2021_05_25_125730_drop_building_id_floors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateFloorsTable extends Migration
+class DropBuildingIdFloorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,8 @@ class UpdateFloorsTable extends Migration
     public function up()
     {
         Schema::table('floors', function (Blueprint $table) {
-            $table->dropColumn('floor_id');
-            $table->dropColumn('floor_no');
-            $table->dropColumn('size_metre');
-            $table->dropColumn('size_feet');
-            $table->longText('building_id')->after('id'); // building from sensor backend
-            $table->longText('ref_id')->after('building_id'); // floor id from sensor backend
+            $table->dropForeign(['building_id', 'floor_id']);
+            $table->dropColumn('building_id');
         });
     }
 
