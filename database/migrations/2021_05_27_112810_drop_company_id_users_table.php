2793976b10741d4ec7fb3acb22b3d81feed0483f
Dionlee Uy <dionleeuy@gmail.com>
2021-08-09 18:31:36 +0800

    Migration fix #4

--- database/migrations/2021_05_27_112810_drop_company_id_users_table.php
+++ database/migrations/2021_05_27_112810_drop_company_id_users_table.php
@@ -15,7 +15,7 @@ class UpdateUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             // change company_id from int to string (sensor backend customer ID)
-            $table->longText('company_id')->change();
+            $table->dropForeign(['company_id']);
         });
     }
 
