64888b27e880bc310019f2938c51828c661db390
Dionlee Uy <dionleeuy@gmail.com>
2021-11-04 18:49:08 +0800

    Update on initial page loading indicator
    - previous loading indicator was not animating

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,3 +1,11 @@
 <template>
     <router-view></router-view>
 </template>
+
+<script>
+export default {
+    mounted() {
+        document.body.classList.add('app-loaded')
+    }
+}
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,46 +14,14 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" async="true"></script>
 </head>
 <body>
+    <div id="app"></div>
     <input type="hidden" id="sensor_api" value="{{ env('SENSOR_API') }}">
-    <div id="app">
-        <div class="app-loader">
-            <!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
-            <svg width="44" height="44" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="#FF5A09">
-                <g fill="none" fill-rule="evenodd" stroke-width="2">
-                    <circle cx="22" cy="22" r="1">
-                        <animate attributeName="r"
-                            begin="0s" dur="1.8s"
-                            values="1; 20"
-                            calcMode="spline"
-                            keyTimes="0; 1"
-                            keySplines="0.165, 0.84, 0.44, 1"
-                            repeatCount="indefinite" />
-                        <animate attributeName="stroke-opacity"
-                            begin="0s" dur="1.8s"
-                            values="1; 0"
-                            calcMode="spline"
-                            keyTimes="0; 1"
-                            keySplines="0.3, 0.61, 0.355, 1"
-                            repeatCount="indefinite" />
-                    </circle>
-                    <circle cx="22" cy="22" r="1">
-                        <animate attributeName="r"
-                            begin="-0.9s" dur="1.8s"
-                            values="1; 20"
-                            calcMode="spline"
-                            keyTimes="0; 1"
-                            keySplines="0.165, 0.84, 0.44, 1"
-                            repeatCount="indefinite" />
-                        <animate attributeName="stroke-opacity"
-                            begin="-0.9s" dur="1.8s"
-                            values="1; 0"
-                            calcMode="spline"
-                            keyTimes="0; 1"
-                            keySplines="0.3, 0.61, 0.355, 1"
-                            repeatCount="indefinite" />
-                    </circle>
-                </g>
-            </svg>
+    <div class="app-loader">
+        <div class="sk-folding-cube">
+            <div class="sk-cube1 sk-cube"></div>
+            <div class="sk-cube2 sk-cube"></div>
+            <div class="sk-cube4 sk-cube"></div>
+            <div class="sk-cube3 sk-cube"></div>
         </div>
     </div>
 
